9870e5ee73aa7d27fedfa9d9abca4804bcf9b2a4
SoumilGoyal <84224690+SoumilGoyal@users.noreply.github.com>
2021-11-19 22:59:33 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,18 @@
+class Paper{
+
+   constructor(x,y,){
+        var options={
+            isStactic:false,
+            restitution:0.3,
+            friction:0,
+            denstity:1.2
+        }
+
+        this.image=loadImage("paper.png");
+        
+
+   }
+    
+    
+
+}
